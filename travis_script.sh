707aca003d5df5d20b82dfd128020fa5d6f600d5
MelissaDaCosta <mel-dacosta@hotmail.fr>
2020-04-25 18:24:38 +0200

    update travis script

--- travis_script.sh
+++ travis_script.sh
@@ -6,15 +6,16 @@ if [ "$FOLDER" = "lab6" ]; then
         tar xvf jdk-15-vector-osx.tar.gz
     else
         # download jdk-15
-        wget https://github.com/forax/java-next/releases/download/untagged-bf24edb7ff6b12ce0d49/jdk-15-vector-osx.tar.gz
+        wget https://github.com/forax/java-next/releases/download/untagged-bf24edb7ff6b12ce0d49/jdk-15-vector-linux.tar.gz
         # extract
-        tar xvf jdk-15-vector-osx.tar.gz
+        tar xvf jdk-15-vector-linux.tar.gz
     fi
     # export new environment variable JAVA_HOME
     export JAVA_HOME=$(pwd)/jdk-15-vector/
 fi
 
 cd $FOLDER
+echo $JAVA_HOME
 mvn package
 
 # lauchn benchmark :
