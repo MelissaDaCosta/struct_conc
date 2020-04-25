47a44655148dcf23e94abdc36089c0b7bb5cf694
MelissaDaCosta <mel-dacosta@hotmail.fr>
2020-04-25 18:32:24 +0200

    update travis script

--- travis_script.sh
+++ travis_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e # exit if return != 0
-if [ "$FOLDER" = "lab6" ]; then
+if [ "$FOLDER" = "lab5" ]; then
     if [ "$OSTYPE" = "darwin17" ]; then   # OS = osx
         wget https://github.com/forax/java-next/releases/download/untagged-bf24edb7ff6b12ce0d49/jdk-15-vector-osx.tar.gz
         tar xvf jdk-15-vector-osx.tar.gz
